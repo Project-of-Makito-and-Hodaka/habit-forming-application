f3cc35f4a1a538ca330901d885effd9fc02f5eed
Hodaka-A <anxisuigao@gmail.com>
2026-02-08 19:58:33 +0900

    refactor:修正

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import Todo from './components/todo/Todo'
 import { BrowserRouter } from "react-router"
 
 function App() {
-  
+
   return (
-    <>
+    <BrowserRouter>
       <Todo />
-    </>
+    </BrowserRouter>
   )
 }
 
